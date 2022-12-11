92adbc09537d4fc0b173bcf4a5658776e7d0633c
williamrosales <andersrosales0207@gmail.com>
2022-12-11 22:32:18 +0100

    Deleted comments

--- src/components/Song.js
+++ src/components/Song.js
@@ -39,18 +39,15 @@ function Song({ text, song, songs, setSongs }) {
         <div className="song-btns">
             <li className={`song-item ${song.added ? "added" : song.edited ? "edited" : ''} `}>{text}</li>
             <button onClick={addSong} className="complete-btn">
-                {/* <i className="fas fa-check"></i> */}
                 <FaCheck />
             </button>
 
             <button onClick={deleteSong} className="trash-btn">
-                {/* <i className="fas fa-trash"></i> */}
                 <FaTrash />
             </button>
 
 
             <button onClick={editSong} className="edit-btn">
-                {/* <i className="fas fa-cog"></i> */}
                 <FaCog />
             </button>
         </div>
